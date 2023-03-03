cc0f82027fc2e84e937f0c4676c54518e9d591bb
John Jezon Barlisan Ajias <johnjezonajias@gmail.com>
2023-03-03 18:10:04 +0800

    Update navigation-mobile.php

--- kickass-child/components/navigation-mobile.php
+++ kickass-child/components/navigation-mobile.php
@@ -9,10 +9,17 @@
 <div id="top-sidebar-wrap">
 	<div class="sidebar-wrap wrap">
 		<nav id="mobile-navigation" class="mobile-navigation" role="navigation">
-			<?= wp_nav_menu(['theme_location' => 'primary-menu', 'menu_id' => 'primary-menu', 'menu_class' => 'sf-menu sf-vertical', 'depth' => 3]) ?>
+			<?php
+			echo wp_nav_menu([
+				'theme_location' => 'primary-menu',
+				'menu_id' 	 => 'primary-menu',
+				'menu_class' 	 => 'sf-menu sf-vertical',
+				'depth' 	 => 3
+			])
+			?>
 		</nav><!-- #mobile-navigation -->
 		<div class="copyright-sidebar">
-			Copyright &copy; <?= date('Y') ?? '' ?> <a href="<?= get_site_url() ?>"><?= trim(str_replace(['http://', 'https://', 'localhost', '~eteam', '24cms.com', 'webdev.john', '//'], '', get_site_url()), '') ?></a><br>
+			Copyright &copy; <?php echo date('Y') ?? '' ?> <a href="<?php echo get_site_url() ?>"><?php echo trim(str_replace(['http://', 'https://', 'localhost', '~eteam', '24cms.com', 'webdev.john', '//'], '', get_site_url()), '') ?></a><br />
 			All rights reserved.
 		</div><!-- .copyright-sidebar -->
 	</div>
