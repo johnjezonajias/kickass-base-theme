11fe31c9ed3881df1abc0ad96513abc740bdfac3
John Jezon Barlisan Ajias <johnjezonajias@gmail.com>
2023-03-03 18:07:29 +0800

    Update navigation-main.php

--- kickass-child/components/navigation-main.php
+++ kickass-child/components/navigation-main.php
@@ -9,13 +9,13 @@
 <div id="header-nav-wrap">
 	<nav id="site-navigation" class="main-navigation" role="navigation">
 		<?php
-			// Set navigation menu
-			wp_nav_menu( 
-				array( 
-					'theme_location' => 'primary-menu',
-					'menu_id' 		 => 'primary-menu',
-				)
-			);
+		// Set navigation menu
+		wp_nav_menu( 
+			array( 
+				'theme_location' => 'primary-menu',
+				'menu_id' 		 => 'primary-menu',
+			)
+		);
 		 ?>
 	</nav><!-- #site-navigation -->
 </div><!-- #header-nav-wrap -->
